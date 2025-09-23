4915df8d8a0efafc0edd8a2b5fc874416d4314a7
Nasim Ahmad <khannasimahmad@gmail.com>
2025-09-23 10:16:04 +0530

    created function to send telegram message

---
+++ comn/sendTelegramMessage.js
@@ -0,0 +1,23 @@
+import axios from "axios";
+import { config } from "dotenv";
+config()
+const BOT_TOKEN = process.env.TELEGRAM_BOT_TOKEN_HERE;
+const CHAT_ID = "6211199995";
+const MESSAGE = "Hello Nasim 👋 from Node.js!";
+
+export async function sendTeleGramMessage(message) {
+
+    const url = `https://api.telegram.org/bot${BOT_TOKEN}/sendMessage`;
+
+    try {
+        const res = await axios.post(url, {
+            chat_id: CHAT_ID,
+            text: message,
+        });
+        // console.log("✅ Message sent:", res.data);
+    } catch (err) {
+        console.error("❌ Error:", err.response?.data || err.message);
+    }
+}
+
+// sendMessage("Hello Nasim 👋 from Node.js!");
